b3a3c3613cc22887def0f45f47afdc12470d6156
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-20 10:58:31 +1000

    Update KeyManager to use .env for asymmetric keys.

--- libs/KeyManager.py
+++ libs/KeyManager.py
@@ -7,26 +7,20 @@ class KeyManager:
     def generate_server_key_pair():
         private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         public_key = private_key.public_key()
-
-        # read json database and serialise it to a dictionary
-        try:
-            with open("keypair.json", "r") as f:
-                keys_db = json.load(f)
-        except FileNotFoundError:
-            keys_db = {}
         
         # serialise the private and public keys to PEM (human readable format)
-        keys_db["private_key"] = private_key.private_bytes(
+        private_key_pem = private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
         ).decode()
 
-        keys_db["public_key"] = public_key.public_bytes(
+        public_key_pem = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ).decode()
 
-        # Update by rewriting the local database
-        with open("keypair.json", "w") as f:
-            json.dump(keys_db, f, indent=4)
+        # Update by rewriting the .env file
+        with open(".env", "w") as f:
+            f.write(f"PRIVATE_KEY={private_key_pem}\n")
+            f.write(f"PUBLIC_KEY={public_key_pem}\n")
