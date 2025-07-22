6abf3a96d19628756678099ff9287ab6fee7e604
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-22 15:51:14 +1000

    Create ThreadFactory that handles the creation of new threads.

---
+++ libs/threads/ThreadFactory.py
@@ -0,0 +1,12 @@
+from libs.Requests.Request import Request
+from libs.threads.NewSessionThread import NewSessionThread
+from server import Server
+
+class ThreadFactory:
+    @staticmethod
+    def create_thread(request: Request, server: Server):
+        match request.get_command():
+            case "START_SESSION":
+                return NewSessionThread(request, server)
+            case _:
+                return
