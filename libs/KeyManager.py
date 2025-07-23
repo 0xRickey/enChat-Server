5885386779d91f7c0e039f493c924d0abcc5408b
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-23 22:22:54 +1000

    refactor keymanager to use DER files instead of using .env files

--- libs/KeyManager.py
+++ libs/KeyManager.py
@@ -1,39 +1,38 @@
-import os, dotenv
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
 
 class KeyManager:
     def __init__(self):
-        if dotenv.load_dotenv():
-            self.private_key = os.getenv("PRIVATE_KEY")
-            self.public_key = os.getenv("PUBLIC_KEY")
-        else:
+        try:
+            with open("keys/private_key.der", "rb") as f:
+                print("Loading private key from DER file")
+                privKeyAsbytes = f.read()
+                self.private_key = serialization.load_der_private_key(
+                    privKeyAsbytes, password=None
+                )
+                self.public_key  = self.private_key.public_key()
+        except FileNotFoundError:
+            print("No private key found. Generating a new one.")
+            
             self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
             self.public_key = self.private_key.public_key()
 
-            priv_key_pem = self.private_key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption()
-            ).decode()
+            with open("keys/private_key.der", "wb") as f:
+                f.write(self.private_key.private_bytes(
+                    encoding=serialization.Encoding.DER,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption()
+                ))
 
-            pub_key_pem = self.public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo
-            ).decode()
-
-            dotenv.set_key(".env", "PRIVATE_KEY", priv_key_pem)
-            dotenv.set_key(".env", "PUBLIC_KEY", pub_key_pem)
+            with open("keys/public_key.der", "wb") as f:
+                f.write(self.public_key.public_bytes(
+                    encoding=serialization.Encoding.DER,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                ))
 
     def get_private_key(self) -> RSAPrivateKey:
         return self.private_key
     
     def get_public_key(self) -> RSAPublicKey:
         return self.public_key
-    
-    def get_pub_key_PEM(self) -> str:
-        return self.public_key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ).decode()
