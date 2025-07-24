a6b72d1f7dfcf4b8f69468411f8f01725aad8c6a
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-24 18:33:57 +1000

    Update response factory to create responses that comply with the new messaging format.

--- libs/response/ResponseFactory.py
+++ libs/response/ResponseFactory.py
@@ -1,7 +1,4 @@
-import uuid, json
-
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 
 from libs.response.Response import Response
 from libs.KeyManager import KeyManager
@@ -12,15 +9,15 @@ class ResponseFactory:
         status: str,
         payload: dict,
         metadata: dict,
-        keyManager: KeyManager
+        keyManager: KeyManager,
     ) -> Response:
-        response_id = str(uuid.uuid4())
-        signature = keyManager.get_private_key().sign(
-            json.dumps(payload).encode(),
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
-        )
-        return Response(status, payload, metadata, signature, response_id)
+        msg  = {
+            "STATUS": status,
+            "PAYLOAD": payload,
+            "METADATA": metadata,
+        }
+
+        signature: str = keyManager.sign_message(msg)
+        rsaPubKey: RSAPublicKey = keyManager.get_public_key()
+
+        return Response(status, payload, metadata, signature, rsaPubKey)
