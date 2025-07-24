743277166f6376a3c4481d1a81c1ca06de48a4a7
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-24 18:33:30 +1000

    Update Response to comply with the new messaging format. It now includes pubKey and init_vec fields in its initialisation.

--- libs/response/Response.py
+++ libs/response/Response.py
@@ -1,18 +1,30 @@
-import json
+import json, uuid
+
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPrivateKey
+from cryptography.hazmat.primitives import serialization
 
 class Response:
-    def __init__(self,
+    def __init__(
+        self,
         status: str,
         payload: dict,
         metadata: dict,
         signature: str,
-        response_id: str
+        pubKey: RSAPublicKey,
+        init_vec: bytes = None
     ):
+        # encrypted and included in the reply to client
         self.status = status
         self.payload = payload
         self.metadata = metadata
-        self.signature = signature
-        self.response_id = response_id
+
+        # unencrypted and included in the reply to client
+        self.signature: str = signature
+        self.pubKey: RSAPublicKey = pubKey
+        self.init_vec: bytes = init_vec
+
+        # serverside logging, not included in reply to client
+        self.response_id = str(uuid.uuid4())
 
     def get_status(self):
         return self.status
@@ -26,15 +38,40 @@ class Response:
     def get_response_id(self):
         return self.response_id
     
+    def get_init_vec(self) -> bytes:
+        return self.init_vec
+
+    def set_init_vec(self, init_vec: bytes):
+        self.init_vec = init_vec
+
+    def get_pub_key(self) -> RSAPublicKey:
+        return self.pubKey
+
+    def get_PEM_pub_key(self):
+        return self.pubKey.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode()
+
+    def set_pub_key(self, pubKey: RSAPublicKey):
+        self.pubKey = pubKey
+    
     def get_signature(self):
         return self.signature
     
-    def as_dict(self):
+    def get_message(self):
         return {
             "STATUS": self.status,
             "PAYLOAD": self.payload,
-            "METADATA": self.metadata,
-            "SIGNATURE": self.signature
+            "METADATA": self.metadata
+        }
+
+    def as_dict(self):
+        return {
+            "MESSAGE": self.get_message(),
+            "SIGNATURE": self.signature,
+            "PUBLIC_KEY": self.pubKey,
+            "IV": self.init_vec
         }
 
     def as_bytes(self):
