160ad61fe202c379c9d650e80552ec68f3acfcbd
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-25 23:47:19 +1000

    Update Decryptor
    Implemented the function AES_decrypt for decrypting AES encrypted requests.

--- libs/Decryptor.py
+++ libs/Decryptor.py
@@ -50,10 +50,46 @@ class Decryptor:
             encryptedRequest.get_return_addr()
         )
 
-    def AES_decrypt(self, encryptedRequest: EncryptedRequest) -> Request:
+    def AES_decrypt(
+        self,
+        encryptedRequest: EncryptedRequest,
+        sessionKey: bytes
+    ) -> Request:
         """
         Decrypts an encrypted request using AESCBC, an initialisation
         vector and a session key that was previously shared with the
         server.
         """
-        pass
+        ciphertext = encryptedRequest.get_ciphertext() # UTF-8 String of base64 bytes
+        ciphertext = ciphertext.encode() # UTF-8 string of base64 bytes -> base64 bytes
+        ciphertext = base64.b64decode(ciphertext) # base64 bytes -> regular mumbo jumbo
+
+        init_vec = encryptedRequest.get_init_vec() # UTF-8 string of base64 bytes
+        init_vec = init_vec.encode() # UTF-8 string or base64 bytes -> base64 bytes
+        init_vec = base64.decode(ciphertext) # base64 bytes -> original mumbo jumbo
+
+        aesCipher = Cipher(
+            algorithm=algorithms.AES(sessionKey),
+            mode=modes.CBC(init_vec)
+        )
+
+        aesDecryptor = aesCipher.decryptor()
+
+        paddedMsg = aesDecryptor.update(ciphertext) + aesDecryptor.finalize()
+
+        unpadder = padding_primitives.PKCS7(constants.AES_BLOCK_SIZE_BITS).unpadder()
+
+        msgJsonBytes: bytes = unpadder.update(paddedMsg) + unpadder.finalize()
+        msgJsonStr: str = msgJsonBytes.decode()
+        msg: dict = json.loads(msgJsonStr)
+
+        request = RequestFactory.request_from_bytes(
+            msgJsonBytes,
+            encryptedRequest.get_signature(),
+            encryptedRequest.get_PEM_public_key(),
+            encryptedRequest.get_init_vec(),
+            encryptedRequest.get_session_id(),
+            encryptedRequest.get_return_addr()
+        )
+
+        return request
