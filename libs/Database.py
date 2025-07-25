5083b6554ac61f631cc88d24f4fd225ac8f833b9
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-25 23:39:31 +1000

    Create new Database class
    New class that acts as an interface between the json database and the code.

---
+++ libs/Database.py
@@ -0,0 +1,11 @@
+import json
+
+class Database:
+    def __init__(self):
+        with open("database.json", "r") as f:
+            database: dict = json.loads(f.read())
+
+        self.users: dict = database["USERS"]
+
+    def does_user_exist(self, username: str):
+        return username in self.users
