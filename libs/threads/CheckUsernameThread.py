8ba693a5dada6e8b05c2dbc8ee0d427822b96537
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-25 23:42:35 +1000

    Create new CheckUsernameThread class
    New class for handling the server side operations that involve checking if a username is available or not

---
+++ libs/threads/CheckUsernameThread.py
@@ -0,0 +1,70 @@
+import time, socket
+
+import libs.constants as constants
+
+
+from threading import Thread, Lock
+
+from libs.requests.Request import Request
+from libs.sessions.SessionsLog import SessionsLog
+from libs.sessions.Session import Session
+from libs.response.ResponseLog import ResponseLog
+from libs.response.Response import Response
+from libs.response.ResponseFactory import ResponseFactory
+from libs.KeyManager import KeyManager
+from libs.Encryptor import Encryptor
+from libs.Database import Database
+
+class CheckUsernameThread(Thread):
+    def __init__(
+        self,
+        request: Request,
+        threadLock: Lock,
+        serverUdpSocket: socket.socket,
+        sessionsLog: SessionsLog,
+        responseLog: ResponseLog,
+        keyManager: KeyManager,
+        encryptor: Encryptor
+    ):
+        super().__init__()
+        self.request: Request = request
+        self.threadLock: Lock = threadLock
+        self.serverUdpSocket: socket.socket = serverUdpSocket
+        self.sessionsLog: SessionsLog = sessionsLog
+        self.responseLog: ResponseLog = responseLog
+        self.keyManager: KeyManager = keyManager
+        self.encryptor: Encryptor = encryptor
+
+    def run(self):
+        print(f"The client has requested to check the availability of a username")
+        with self.threadLock:
+            database: Database = Database()
+            username: str = self.request.get_payload()["USERNAME"]
+            
+            if database.does_user_exist(username):
+                status = constants.USERNAME_TAKEN
+            else:
+                status = constants.USERNAME_AVAILABLE
+
+            response: Response = ResponseFactory.create_response(
+                status=status,
+                payload={},
+                metadata={},
+                keyManager=self.keyManager
+            )
+
+            session: Session = self.sessionsLog.get_session(
+                self.request.get_session_id()
+            )
+
+            aesEncryptedResBytes: bytes = self.encryptor.AES_encrypt(
+                response=response,
+                session_key=session.get_expanded_session_key()
+            )
+
+            self.responseLog.add_response(response)
+
+            self.serverUdpSocket.sendto(
+                aesEncryptedResBytes,
+                self.request.get_return_addr()
+            )
