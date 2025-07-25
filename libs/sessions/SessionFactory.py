a87cb8d642433b6f80bc325f15d36d073cccc122
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-25 11:31:15 +1000

    Update SessionFactory.py
    Sessions now use integers for the session_id and session_key.

--- libs/sessions/SessionFactory.py
+++ libs/sessions/SessionFactory.py
@@ -4,6 +4,6 @@ from libs.sessions.Session import Session
 class SessionFactory:
     @staticmethod
     def new_session(request: Request) -> Session:
-        session_id: str = request.get_payload()["SESSION_ID"]
-        session_key: bytes = request.get_payload()["SESSION_KEY"].encode()
+        session_id: int = request.get_payload()["SESSION_ID"]
+        session_key: int = request.get_payload()["SESSION_KEY"]
         return Session(session_id, session_key)
