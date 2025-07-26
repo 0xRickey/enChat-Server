bc55d83a3777dd40215187f2f69f03847b649595
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-26 15:51:34 +1000

    Update Database
    - Added new function add_user() that registers a user using their provided username and password but the password is stored as a salted hash.

--- libs/Database.py
+++ libs/Database.py
@@ -1,4 +1,4 @@
-import json
+import json, hashlib, os
 
 class Database:
     def __init__(self):
@@ -9,3 +9,15 @@ class Database:
 
     def does_user_exist(self, username: str):
         return username in self.users.keys()
+    
+    def add_user(self, username: str, password: str):
+        THIRTY_TWO_BYTES = 32
+        # Generate 32 random bytes to use a password salt
+        salt: str = os.urandom(THIRTY_TWO_BYTES).hex()
+        saltedPassword: str = salt + password
+        saltedPwdHash: str = hashlib.sha256(saltedPassword).hexdigest()
+
+        self.users[username] = {
+            "SALT": salt,
+            "PASSWORD": saltedPwdHash
+        }
