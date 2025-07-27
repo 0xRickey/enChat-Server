34edc693b229381cefe66e54440957414b5d8b39
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-27 15:42:11 +1000

    Create new LogoutThread class

---
+++ libs/threads/LogoutThread.py
@@ -0,0 +1,67 @@
+import time, socket
+
+import libs.constants as constants
+
+from threading import Thread, Lock
+
+from libs.requests.Request import Request
+from libs.sessions.SessionsLog import SessionsLog
+from libs.sessions.Session import Session
+from libs.response.ResponseLog import ResponseLog
+from libs.response.Response import Response
+from libs.response.ResponseFactory import ResponseFactory
+from libs.KeyManager import KeyManager
+from libs.Encryptor import Encryptor
+from libs.Database import Database
+
+class LoginThread(Thread):
+    def __init__(
+        self,
+        request: Request,
+        threadLock: Lock,
+        serverUdpSocket: socket.socket,
+        sessionsLog: SessionsLog,
+        responseLog: ResponseLog,
+        keyManager: KeyManager,
+        encryptor: Encryptor
+    ):
+        super().__init__()
+        self.request: Request = request
+        self.threadLock: Lock = threadLock
+        self.serverUdpSocket: socket.socket = serverUdpSocket
+        self.sessionsLog: SessionsLog = sessionsLog
+        self.responseLog: ResponseLog = responseLog
+        self.keyManager: KeyManager = keyManager
+        self.encryptor: Encryptor = encryptor
+
+    def run(self):
+        print("\nThe client has issued an EXIT command")
+        
+        with self.threadLock:
+            session_id: int = self.request.get_metadata()["SESSION_ID"]
+            self.sessionsLog.remove_session(session_id)
+
+            response: Response = ResponseFactory.create_response(
+                status=constants.LOGOUT_SUCCESS,
+                payload={},
+                metadata={},
+                keyManager=self.keyManager
+            )
+
+            self.responseLog.add_response(response)
+
+            rsaEncryptedResBytes: bytes = self.encryptor.RSA_encrypt(
+                response=response,
+                PEM_pub_key=response.get_PEM_pub_key()
+            )
+
+            self.serverUdpSocket.sendto(
+                rsaEncryptedResBytes,
+                self.request.get_return_addr()
+            )
+
+        print("Successfully removed the client's session. User is now logged out")
+        print("Sending a response back to the client")
+
+
+            
