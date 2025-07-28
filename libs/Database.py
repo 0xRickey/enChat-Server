13262911e463229c339b8df19c2384b8af24ecff
Rickey <95059419+0xRickey@users.noreply.github.com>
2025-07-28 13:06:25 +1000

    Update database
    - Add ability to create the database.json file if it doesn't exist
    - New function get_messages. Gets the messages from userA to userB based on when the last get request was
    - update add_msg. It will now create a new chat with the "last_get" and "messages" field if the chat doesn't exist

--- libs/Database.py
+++ libs/Database.py
@@ -1,14 +1,18 @@
-import json, hashlib, os
+import json, hashlib, os, time
 
 from libs.Message import Message
 
 class Database:
     def __init__(self):
-        with open("database.json", "r") as f:
-            database: dict = json.loads(f.read())
+        try:
+            with open("database.json", "r") as f:
+                database: dict = json.loads(f.read())
 
-        self.users: dict = database["USERS"]
-        self.chats: dict = database["CHATS"]
+            self.users: dict = database["USERS"]
+            self.chats: dict = database["CHATS"]
+        except FileNotFoundError:
+            self.users: dict = {}
+            self.chats: dict = {}
 
     def does_user_exist(self, username: str):
         return username in self.users.keys()
@@ -40,14 +44,33 @@ class Database:
         with open("database.json", "w") as f:
             f.write(json.dumps({"USERS": self.users, "CHATS": self.chats}, indent=4))
 
-    def add_msg(self, fromUser: str, toUser: str, msg: str):
+    def add_msg(self, fromUser: str, toUser: str, msg: str, timestamp: int):
         chatKey = f"{fromUser}-{toUser}"
-        newMsg: Message = Message(f"{fromUser}: {msg}")
+        newMsg: Message = Message(msg=msg, timestamp=timestamp)
 
         try:
-            chatMessages: list[(str, int)] = self.chats[chatKey]
+            chatMessages: list[dict] = self.chats[chatKey]["MESSAGES"]
             chatMessages.append(newMsg.as_dict())
         except KeyError:
-            self.chats[chatKey] = [newMsg.as_dict()]
+            self.chats[chatKey] = {
+                "LAST_GET": 0,
+                "MESSAGES": [newMsg.as_dict()]
+            }
 
         self.write_to_db()
+
+    def get_messages(self, fromUser: str, toUser: str) -> list[Message] | None:
+        try:
+            chatKey = f"{fromUser}-{toUser}"
+            newMsgs: list[Message] = []
+            lastGet: int = self.chats[chatKey]["LAST_GET"]
+            for msg in self.chats[chatKey]["MESSAGES"]:
+                if msg["TIMESTAMP"] > lastGet:
+                    newMsgs.append(Message(msg["MESSAGE"], msg["TIMESTAMP"]))
+            
+            self.chats[chatKey]["LAST_GET"] = int(time.time())
+            
+            self.write_to_db()
+            return newMsgs
+        except KeyError:
+            return None
